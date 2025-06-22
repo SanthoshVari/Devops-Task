a27cf5e32ae0218da8bfa0999a3dd5b11afa084a
SanthoshVari <santhosh.devopscloud@gmail.com>
2025-06-22 20:03:18 +0530

    terraform

--- Terraform/main.tf
+++ Terraform/main.tf
@@ -39,7 +39,7 @@ module "vpc" {
 
 module "eks" {
   source = "./modules/eks"
-
+  region = var.region
   cluster_name    = var.cluster_name
   cluster_version = var.cluster_version
   vpc_id          = module.vpc.vpc_id
