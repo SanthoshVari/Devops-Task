2de60fb8b5589c1489a235d335ba0059c06d1f5b
SanthoshVari <santhosh.devopscloud@gmail.com>
2025-06-22 19:55:10 +0530

    terraform

--- Terraform/main.tf
+++ Terraform/main.tf
@@ -7,7 +7,7 @@ terraform {
   }
 
   backend "s3" {
-    bucket         = "cluster1-terraform-eks-state-s3-bucket1"
+    bucket         = "devops-task-terraform-state-locking"
     key            = "terraform.tfstate"
     region         = "ap-south-1"
     dynamodb_table = "terraform-eks-state-locks"
