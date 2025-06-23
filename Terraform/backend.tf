e51703952027c272c69b8ad6ffa68c05a0fc10f6
SanthoshVari <santhosh.devopscloud@gmail.com>
2025-06-23 16:51:59 +0530

    terraform with cloudwatch

--- Terraform/backend.tf
+++ Terraform/backend.tf
@@ -1,6 +1,6 @@
 terraform {
   backend "s3" {
-    bucket         = "${var.state_bucket_name}-${random_string.bucket_suffix.result}"
+    bucket         = "flask-eksdevops-task"
     key            = "terraform/eks-flask-app/state.tfstate"
     region         = "ap-south-1"
     dynamodb_table = "terraform-locks"
