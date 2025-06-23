0e2a147e6232d89e0cf7ab70e603911e2ba31d9b
SanthoshVari <santhosh.devopscloud@gmail.com>
2025-06-23 18:32:50 +0530

    terraform dynamodb edit

--- Terraform/main.tf
+++ Terraform/main.tf
@@ -18,18 +18,18 @@ resource "aws_s3_bucket_versioning" "terraform_state_versioning" {
   }
 }
 
-resource "aws_dynamodb_table" "terraform_locks" {
-  name           = var.dynamodb_table_name
-  billing_mode   = "PAY_PER_REQUEST"
-  hash_key       = "LockID"
-  attribute {
-    name = "LockID"
-    type = "S"
-  }
-  tags = {
-    Name = "terraform-locks"
-  }
-}
+# resource "aws_dynamodb_table" "terraform_locks" {
+#   name           = var.dynamodb_table_name
+#   billing_mode   = "PAY_PER_REQUEST"
+#   hash_key       = "LockID"
+#   attribute {
+#     name = "LockID"
+#     type = "S"
+#   }
+#   tags = {
+#     Name = "terraform-locks"
+#   }
+# }
 
 module "vpc" {
   source = "./modules/vpc"
