5920501b712cf2275b6b95371db6cc6684b46be3
SanthoshVari <santhosh.devopscloud@gmail.com>
2025-06-27 14:10:49 +0530

    CI/CD improvements

--- Terraform/modules/iam/variables.tf
+++ Terraform/modules/iam/variables.tf
@@ -1,6 +1,6 @@
-output "github_actions_role_arn" {
-  value = aws_iam_role.github_actions_role.arn
-}
+# output "github_actions_role_arn" {
+#   value = aws_iam_role.github_actions_role.arn
+# }
 
 # Variables
 variable "region" {
